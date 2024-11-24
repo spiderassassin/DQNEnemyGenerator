4027c6c7524b76d79c6bacbe7af91138e620aae5
Jawdat <57550053+JawdatToume@users.noreply.github.com>
2024-11-23 19:39:19 -0700

    Added distance to logger

--- PacManUnity/Assets/Logger.cs
+++ PacManUnity/Assets/Logger.cs
@@ -8,12 +8,11 @@ public class Logger : MonoBehaviour
     public GameHandler gameHandler;
     public string filePath = "log.csv";
     private float nextTime = 0.0f;
-    private GameObject ghost;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        File.WriteAllText(filePath, "Time, Pellets, Tension, Position, Reward\n");
+        File.WriteAllText(filePath, "Time, Pellets, Tension, Position, Distance, Reward\n");
     }
 
     // Update is called once per frame
@@ -22,7 +21,10 @@ public class Logger : MonoBehaviour
         if(Time.time > nextTime){
             nextTime += 1.0f;
             Vector3 position = gameHandler.GetState().ghostPosition;
-            string text = string.Format("{0}, {1}, {2}, ({3} {4} {5}), {6}\n", Time.time, pelletHandler.NumPellets, gameHandler.accTension, position[0], position[1], position[2], gameHandler.currReward);
+            Vector3 pacman = gameHandler.GetState().agentPosition;
+            float distance = Math.Abs(position[0]-pacman[0]) + Math.Abs(position[1]-pacman[1]) + Math.Abs(position[2]-pacman[2]);
+
+            string text = string.Format("{0}, {1}, {2}, ({3} {4} {5}), {6}, {7}\n", Time.time, pelletHandler.NumPellets, gameHandler.accTension, position[0], position[1], position[2], distance, gameHandler.currReward);
             print(text);
             File.AppendAllText(filePath, text);
         }
