48a5815456350fb56d848dda04b1d1f41cfb41f8
Jawdat <57550053+JawdatToume@users.noreply.github.com>
2024-11-30 15:51:51 -0700

    Evaluator update

--- evaluator.py
+++ evaluator.py
@@ -4,16 +4,48 @@ import os
 import csv
 import matplotlib.pyplot as plt
 
-def peaks(number, results, directory):
+def peaks(text, results, directory):
     infos = ["time", "pellets", "tension", "distance", "reward"]
 
-    for i in range(number):
-        for info in infos[1:]:
-            plt.scatter(results[str(i)]["time"], results[str(i)][info])
-            print(scipy.signal.find_peaks(results[str(i)][info]))
-            plt.savefig(info + " " + str(i) + " " +  directory.replace('/','-'))
+    text2 = open("eval//stats//" + directory.replace('/','-') + "peaks.txt", 'a')
+    numPeaks = {}
+    numPeaks["before"] = []
+    numPeaks["after"] = []
 
-def change(number, results, directory):
+    for key in results["before"].keys():
+        for info in infos[2:3]:
+            plt.plot(results["before"][key]["time"], results["before"][key][info])
+            text2.write(info + " peaks of initial game " + key + "\n")
+            peaks = scipy.signal.find_peaks(results["before"][key][info])
+            text2.write(str(peaks))
+            text2.write("\n")
+            numPeaks["before"].append(len(peaks[0]))
+            plt.xlabel("time")
+            plt.ylabel(info)
+            plt.title("Graph for " + info + " vs. time from initial game " + key)
+            plt.savefig(os.path.join("eval//graphs//line//" + info + "//before", info + "-" + "-before-" + key + "-" +  directory.replace('/','-')))
+            plt.clf()
+    
+    for key in results["after"].keys():
+        for info in infos[2:3]:
+            plt.plot(results["after"][key]["time"], results["after"][key][info])
+            text2.write(info + " peaks of final game " + key + "\n")
+            peaks = scipy.signal.find_peaks(results["after"][key][info])
+            text2.write(str(peaks))
+            text2.write("\n")
+            numPeaks["after"].append(len(peaks[0]))
+            plt.xlabel("time")
+            plt.ylabel(info)
+            plt.title("Graph for " + info + " vs. time from final game " + key)
+            plt.savefig(os.path.join("eval//graphs//line//" + info + "//after", info + "-" + "-after-" + key + "-" +  directory.replace('/','-')))
+            plt.clf()
+
+    text2.close()
+    text.write("change in peaks\n")
+    text.write(str(scipy.stats.wilcoxon(numPeaks["before"], numPeaks["after"])))
+    text.write("\n")
+
+def change(text, results, directory):
     infos = ["time", "pellets", "tension", "distance", "reward"]
     before = {}
     after = {}
@@ -22,24 +54,34 @@ def change(number, results, directory):
         before[info] = []
         after[info] = []
 
-    for i in range(int(number/2)):
-        values = results[str(i)]
+    for key in results["before"].keys():
+        values = results["before"][key]
         for info in infos:
+            if len(values[info]) == 0:
+                continue
             before[info].append(values[info][-1])
             
-    for i in range(int(number/2), number):
-        values = results[str(i)]
+    for key in results["after"].keys():
+        values = results["after"][key]
         for info in infos:
+            if len(values[info]) == 0:
+                continue
             after[info].append(values[info][-1])
 
-    for info in infos:
+    for info in infos[2:3]:
         plt.boxplot(before[info])
-        plt.savefig(directory)
+        plt.ylabel(info)
+        plt.savefig(os.path.join("eval//graphs//boxplots",directory.replace('/','-')+"before"))
+        plt.clf()
         plt.boxplot(after[info])
-        plt.savefig(directory)
-        print(scipy.stats.ttest_ind(before[info], after[info]))
+        plt.ylabel(info)
+        plt.savefig(os.path.join("eval//graphs//boxplots",directory.replace('/','-')+"after"))
+        plt.clf()
 
-# number is the amount of logs we have (assume first half is first test, second half is second test)
+        text.write("change in " + info + "\n")
+        text.write(str(scipy.stats.wilcoxon(before[info], after[info])))
+        text.write("\n")
+
 def gatherResults(directory):
     logs = [0,0]
     infos = ["time", "pellets", "tension", "distance", "reward"]
@@ -49,50 +91,51 @@ def gatherResults(directory):
         elif i.find("final") > -1:
             logs[1] = i
 
-    # for i in range(number):
-    #     logs.append(open("log" + str(i) + ".csv", "r"))
-
     results = {}
-    currNum = 0
-
+    logNum = 0
     for logFile in logs:
-        results[str(currNum)] = {}
-        for info in infos:
-            results[str(currNum)][info] = []
+        if logNum == 0:
+            state = "before"
+        else:
+            state = "after"
+        results[state] = {}
         log = open(os.path.join(directory, logFile), 'r')
+        currNum = -1
         values = csv.reader(log)
-        print(values)
-        prev_tension = 0
-        prev_pellets = 480810580
         lineNum = 0
         for line in values:
             if lineNum == 0:
                 lineNum += 1
                 continue
-            print(line)
-            if float(line[1]) > prev_pellets:
+            if line[0][:4] == "Game":
                 currNum += 1
-                results[str(currNum)] = {}
+                results[state][str(currNum)] = {}
                 for info in infos:
-                    results[str(currNum)][info] = []
+                    results[state][str(currNum)][info] = []
+                continue
 
-            results[str(currNum)]["time"].append(float(line[0]))
-            results[str(currNum)]["pellets"].append(float(line[1]))
-            results[str(currNum)]["tension"].append(float(line[2]) - prev_tension)
-            results[str(currNum)]["distance"].append(float(line[3]))
-            results[str(currNum)]["reward"].append(float(line[4]))
-            prev_tension = float(line[2])
-            prev_pellets = float(line[1])
+            if currNum > 1000:
+                break
+
+            results[state][str(currNum)]["time"].append(float(line[0]))
+            results[state][str(currNum)]["pellets"].append(float(line[1]))
+            results[state][str(currNum)]["tension"].append(float(line[2]))
+            results[state][str(currNum)]["distance"].append(float(line[3]))
+            results[state][str(currNum)]["reward"].append(float(line[4]))
         log.close()
+        logNum += 1
 
     return results
 
 if __name__ == "__main__":
     directory = sys.argv[1]
-    number = int(sys.argv[2])
+
+    text = open("eval//stats//" + directory.replace('/','-') + ".txt", 'a')
 
     results = gatherResults(directory)
 
-    peaks(number, results, directory)
+    peaks(text, results, directory)
 
-    change(number, results, directory)
+    change(text, results, directory)
+
+    text.close()
